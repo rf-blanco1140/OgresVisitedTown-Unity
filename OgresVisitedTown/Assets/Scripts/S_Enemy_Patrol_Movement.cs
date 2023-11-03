6b28d8bd2144c7ca411331120cf87c754b1746e4
rf-blanco1140 <rf-blanco1140@users.noreply.github.com>
2023-11-03 11:40:25 +0100

    Added basic enemy movement

--- OgresVisitedTown/Assets/Scripts/S_Enemy_Patrol_Movement.cs
+++ OgresVisitedTown/Assets/Scripts/S_Enemy_Patrol_Movement.cs
@@ -5,26 +5,43 @@ using UnityEngine;
 public class S_Enemy_Patrol_Movement : MonoBehaviour
 {
     [SerializeField] private Transform[] patrolPoints;
-    private int nextPoint;
+    [SerializeField]private int nextPoint;
     private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    private Vector2 direction;
+    private Vector2 distanceToNextPoint;
 
     private void Start()
     {
-        nextPoint = 1;
+        nextPoint = 0;
         rb = gameObject.GetComponent<Rigidbody2D>();
+        direction = new Vector2();
+        distanceToNextPoint = new Vector2();
     }
 
     private void Update()
     {
-        if(rb.transform.position == patrolPoints[nextPoint].position)
+        distanceToNextPoint = ((Vector2)patrolPoints[nextPoint].position - (Vector2)rb.transform.position);
+        float xDistance = Mathf.Abs(distanceToNextPoint.x);
+        float yDistance = Mathf.Abs(distanceToNextPoint.y);
+        if (xDistance < 0.1 && yDistance < 0.1)
         {
-            nextPoint++;
+            if(nextPoint == patrolPoints.Length-1)
+            {
+                nextPoint = 0;
+            }
+            else
+            {
+                nextPoint++;
+            }
+            direction = ((Vector2)patrolPoints[nextPoint].position- (Vector2)rb.transform.position ).normalized;
         }
+
         MoveToNextPoint();
     }
 
     private void MoveToNextPoint()
     {
-
+        rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
     }
 }
